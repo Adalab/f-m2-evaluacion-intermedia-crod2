66b8b0a5fed8bf150042d52dd5976c4f57dd8de9
crod2 <carlarodriguez90@gmail.com>
2019-04-21 20:43:24 +0200

    Fix errors

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 
 /* Juego que genera un número al azar que el jugador tiene que adivinar. El juego dará pistas acerca de si el número probado es demasiado alto o bajo, y contabilizará el número de intentos hasta que el juegador acierte. */
 
-const button = document.querySelector('.btn');
 const inputContent = document.querySelector('.input');
+const button = document.querySelector('.btn');
+const text = document.querySelector('.subtext');
 const randomNumber = getRandom(1, 100);
-
+const counter = document.querySelector('.tries');
+let i = 0;
 
 /* Generar un número aleatorio y mostrarlo en la consola */
 
@@ -17,15 +19,25 @@ function getRandom(min, max) {
 console.log(randomNumber);
 
 /* Al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola */
-
-function result() {
-    const defaultContent = inputContent.value;
-    const userNumber = parseInt(defaultContent);
-    console.log(userNumber);
-}
-
-button.addEventListener('click', result);
-
+function game() {
+    const userNumber = parseInt(inputContent.value);
 /* Comparar el número elegido por el usuario con el número aleatorio e imprimir el feedback correspondiente */
+    if (randomNumber === userNumber){
+        text.innerHTML = "¡HAS GANADO, CAMPEONA!";
+    } else if (randomNumber > userNumber) {
+        text.innerHTML = "Tu número es demasiado bajo";
+        counter.innerHTML = i;
+        i = i + 1;
+    } else (randomNumber < userNumber) 
+        text.innerHTML = "Tu número es demasiado alto";
+        counter.innerHTML = i;
+        i = i + 1;
+    }
 
+/* Para escribir el contador: 
+let i = 0, crea un contador y lo inicializa al 0
+y dentro del if especifico que counter debe pintarme i porque representa los intentos del contador
+tb añado i = i+1 para que continúe la suma de los intentos realizados
+*/
 
+button.addEventListener('click', game);
